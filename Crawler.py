cae89985073069f95f2acced63795cfed8dfd735
Brandon <109305092@g.nccu.edu.tw>
2022-06-13 20:56:36 +0800

    feat: get Time and link label

--- Crawler.py
+++ Crawler.py
@@ -79,7 +79,7 @@ for content in contents:
 links = soup.find_all('a', {"class": "oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 gpro0wi8 b1v8xokw"})
 linkNumber = 0
 for link in links: 
-    print("-------------linkNumber-------------", linkNumber)
+    print("-------------link｀Number-------------", linkNumber)
     print(link.get('href'), "連結")
     print(link.get('aria-label'), "時間")
     linkNumber += 1
