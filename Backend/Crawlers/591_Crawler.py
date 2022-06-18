28d866433d61c6d0eebc45afb8075dc207f93512
Brandon <109305092@g.nccu.edu.tw>
2022-06-18 15:26:35 +0800

    feat: 591 -> able to craw all data within Taipei

--- Backend/Crawlers/591_Crawler.py
+++ Backend/Crawlers/591_Crawler.py
@@ -6,6 +6,8 @@ from selenium.webdriver.chrome.options import Options
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -22,70 +24,86 @@ options = Options()
 options.add_argument("--disable-notifications")
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
-firstRow = 0
 chrome = webdriver.Chrome(service=Service(CHROME_PATH), options=options)
-
-
 
 
 contents = []
 postNumber = 0
-has_next_page = True
-while(has_next_page):
-
-    chrome.get(
-        "https://rent.591.com.tw/?region=1&section=12&order=posttime&orderType=desc&firstRow=" + str(firstRow))
-    time.sleep(2)
-
-    soup = BeautifulSoup(chrome.page_source, 'html.parser')
-    posts = soup.find_all('section', {'class': 'vue-list-rent-item'})
-
-    total_rows = soup.find("span", {"class": "R"}).text
-    print(total_rows)
 
 
-    for post in posts:
-        print("-------------postNumber-------------", postNumber)
-        content = {
-            "id_591": "",
-            "title": "",
-            "link": "",
-            "location": "",
-            "price": "",
-            "type": "",
-            "size": ""
-        }
-        # 把內文的每一行都找出來，並印出來
+# section 
+# 1: 中正區
+# 2: 大同區
+# 3: 中山區
+# 4: 松山區
+# 5: 大安區
+# 6: 萬華區
+# 7: 信義區
+# 8: 士林區
+# 9: 北投區
+# 10: 內湖區
+# 11: 南港區
+# 12: 文山區
+for section in range(1, 13):
+    has_next_page = True
+    firstRow = 0
 
-        id_591 = post['data-bind']
-        title = post.find('div', {'class': "item-title"}
-                          ).text.replace(" ", "").replace("\n", "")
-        link = post.find("a", {'target': "_blank"})['href']
-        location = post.find('div', {'class': "item-area"}
-                             ).text.replace(" ", "").replace("\n", "")
-        price = post.find(
-            'div', {'class': "item-price-text"}).text.replace("元/月", "").replace("\n", "").replace(" ", "")
-        type = (post.find("ul", {"class": "item-style"})).decode_contents().split(
-            " ")[0].replace("<li>", "").replace("</li>", "")
-        size = post.find("ul", {"class": "item-style"}).decode_contents().split(" ")[
-            2].replace("<li>", "").replace("</li>", "")
-        # ============= clean Data =============
-        content.update({"id_591": id_591})
-        content.update({"title": title})
-        content.update({"link": link})
-        content.update({"location": location})
-        content.update({"price": price})
-        content.update({"type": type})
-        content.update({"size": size})
-        contents.append(content)
+    while(has_next_page):
+        chrome.get(
+            "https://rent.591.com.tw/?region=1&section={}&order=posttime&orderType=desc&firstRow={}".format(section, str(firstRow)))
+        WebDriverWait(chrome, 1000).until(EC.presence_of_element_located((By.CLASS_NAME, "R")))
+        soup = BeautifulSoup(chrome.page_source, 'html.parser')
 
-        postNumber += 1
-    
-    if(firstRow >= int(total_rows)):
-        has_next_page = False
-        break
-    else:
-        firstRow += 30
+        posts = soup.find_all('section', {'class': 'vue-list-rent-item'})
+        total_rows = soup.find("span", {"class": "R"})
+        print(total_rows)
+        total_rows = total_rows.text
+        print(total_rows)
+
+        for post in posts:
+            print("-------------postNumber-------------", postNumber)
+            content = {
+                "id_591": "",
+                "title": "",
+                "link": "",
+                "section": section,
+                "location": "",
+                "price": "",
+                "type": "",
+                "size": ""
+            }
+            # 把內文的每一行都找出來，並印出來
+
+            id_591 = post['data-bind']
+            title = post.find('div', {'class': "item-title"}
+                            ).text.replace(" ", "").replace("\n", "")
+            link = post.find("a", {'target': "_blank"})['href']
+            location = post.find('div', {'class': "item-area"}
+                                ).text.replace(" ", "").replace("\n", "")
+            price = post.find(
+                'div', {'class': "item-price-text"}).text.replace("元/月", "").replace("\n", "").replace(" ", "")
+            type = (post.find("ul", {"class": "item-style"})).decode_contents().split(
+                " ")[0].replace("<li>", "").replace("</li>", "")
+            size = post.find("ul", {"class": "item-style"}).decode_contents().split(" ")[
+                2].replace("<li>", "").replace("</li>", "")
+            if(size == "-->"):
+                size = "Nan"
+            # ============= clean Data =============
+            content.update({"id_591": id_591})
+            content.update({"title": title})
+            content.update({"link": link})
+            content.update({"location": location})
+            content.update({"price": price})
+            content.update({"type": type})
+            content.update({"size": size})
+            contents.append(content)
+
+            postNumber += 1
+
+        if(firstRow >= int(total_rows)):
+            has_next_page = False
+        else:
+            firstRow += 30
 
 # ============= 連 mongoDB =============
 client = pymongo.MongoClient(MONGO_CONNECTION)
