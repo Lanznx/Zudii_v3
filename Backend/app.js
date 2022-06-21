ad354fb9d6ec020c3e2910a49a845c3e6e2ac456
Brandon <109305092@g.nccu.edu.tw>
2022-06-21 12:45:22 +0800

    feat: create static file access

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,19 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+app.use(express.static(__dirname));
+app.use(express.static("./public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let visitor_number = 0
+app.get('/', (req, res) => {
+  visitor_number++;
+  res.send(`Wazzzaaapppp \nYou are visitor no.${visitor_number}`);
+});
+
+
+
 // 用於辨識 Line Channel 的資訊
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
@@ -11,19 +24,26 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 });
 
+
+serverPort = 4000
+app.listen(serverPort, () => {
+  console.log(`🔥🔥🔥 hi, your server is running on port ${serverPort} 🔥🔥🔥`);
+});
+
+
+
 // 當有人傳送訊息給 line Bot:
 bot.on("message", async (event) => {
   // event.message.text：使用者傳給 linebot 的訊息
   // event.reply(msg) 可以將回傳 msg 給使用者
-  try{
+  try {
     const replyMessage = await search(event.message.text);
     event.reply(replyMessage).then((data) => {
       console.log(data, "=== data ===");
     });
-  }
-  catch(err){
+  } catch (err) {
     console.log(err, "=== err ===");
-    event.reply("不知道你在說什麼")
+    event.reply("不知道你在說什麼");
   }
 });
 
@@ -32,11 +52,9 @@ bot.on("message", async (event) => {
 
 
 
-
-
-
-
-port = 3000;
+port = 5000;
 bot.listen("/linewebhook", port, () => {
   console.log(`🧨🧨🧨🧨🧨🧨🧨🧨 lineBot listen on ${port} 🧨🧨🧨🧨🧨🧨🧨🧨`);
 });
+
+
