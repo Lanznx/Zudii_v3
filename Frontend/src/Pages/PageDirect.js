f0dfc1dd31d994d0fe043c94f76d1ff159a9ef4b
Brandon <109305092@g.nccu.edu.tw>
2022-06-27 17:06:35 +0800

    feat: add direct page

---
+++ Frontend/src/Pages/PageDirect.js
@@ -0,0 +1,60 @@
+import * as React from "react";
+import { Grid, Typography } from "@mui/material";
+import { Button, CardActionArea } from "@mui/material";
+import liff from "@line/liff";
+const REACT_APP_LIFF_DIRECT_ID = process.env.REACT_APP_LIFF_DIRECT_ID;
+
+export default function PageDirect() {
+  const [userId, setUserId] = React.useState("");
+  const [displayName, setDisplayName] = React.useState("");
+
+  async function initializeLIFF() {
+    console.log(REACT_APP_LIFF_DIRECT_ID)
+    await liff.init({
+      liffId: REACT_APP_LIFF_DIRECT_ID, // Use own liffId
+      withLoginOnExternalBrowser: true, // Enable automatic login process
+    });
+
+    // Start to use liff's api
+    console.log("liff initialized");
+    if (!liff.isLoggedIn() && !liff.isInClient()) {
+      window.alert("麻煩登入再來，掰掰！");
+    } else {
+      const profile = await liff.getProfile();
+      setUserId(profile.userId);
+      setDisplayName(profile.displayName);
+
+      console.log(profile, " profile"); // print raw profile object
+    }
+  }
+  initializeLIFF();
+
+  return (
+    <Grid container>
+      <Grid item xs={2} md={2} />
+      <Grid item xs={8} md={8}>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{
+            width: "100%",
+            height: "50px",
+            borderRadius: "3px",
+            backgroundColor: "#4EADCB",
+            color: "white",
+            fontSize: "20px",
+            fontWeight: "bold",
+            marginTop: "20px",
+            marginBottom: "20px",
+          }}
+          onClick={() => {
+            window.location.href = `https://notify-bot.line.me/oauth/authorize?response_type=code&scope=notify&response_mode=form_post&client_id=DhQfj4nguPyCOcJpG9posj&redirect_uri=http://localhost:4500/notify&state=${userId}`;
+          }}
+        >
+          開啟通知
+        </Button>
+      </Grid>
+      <Grid item xs={2} md={2} />
+    </Grid>
+  );
+}
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Page591 from "./Pages/Page591";
+import PageDirect from "./Pages/PageDirect";
 import PageFaceBook from "./Pages/PageFaceBook";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,6 +12,7 @@ root.render(
       <Routes>
         <Route path="/page591" element={<Page591 />} />
         <Route path="/pageFB" element={<PageFaceBook />} />
+        <Route path="/" element={<PageDirect />} />
       </Routes>
     </Router>
   </React.Fragment>
