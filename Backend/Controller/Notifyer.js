7b0c7f2a375e5e05705949d13296d4087800c0f1
Brandon <109305092@g.nccu.edu.tw>
2022-06-27 20:39:07 +0800

    WIP: invalid access token

--- Backend/Controller/Notifyer.js
+++ Backend/Controller/Notifyer.js
@@ -9,7 +9,7 @@ async function setUserAccessToken(req, res) {
   const { code, state } = req.body;
   console.log(process.env.NOTIFY_ID, "process.env.NOTIFY_ID")
   console.log(process.env.NOTIFY_SECRET, "process.env.NOTIFY_SECRET")
-  console.log(req, "REQUEST");
+  console.log(req.response.config.request.data, "REQUEST");
   console.log(code, "setUserAccessTokenModel: CODE");
   console.log(state, "setUserAccessTokenModel: userID");
   axios
