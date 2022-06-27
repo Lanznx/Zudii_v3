ec0b5c6522dfd653060e99c5b696a72d5b60b5f4
Brandon <109305092@g.nccu.edu.tw>
2022-06-27 21:03:36 +0800

    WIP: invalid access token

--- Backend/Controller/Notifyer.js
+++ Backend/Controller/Notifyer.js
@@ -7,21 +7,21 @@ require("dotenv").config();
 
 async function setUserAccessToken(req, res) {
   const { code, state } = req.body;
-  console.log(process.env.NOTIFY_ID, "process.env.NOTIFY_ID")
-  console.log(process.env.NOTIFY_SECRET, "process.env.NOTIFY_SECRET")
+  console.log(process.env.NOTIFY_ID, "process.env.NOTIFY_ID");
+  console.log(process.env.NOTIFY_SECRET, "process.env.NOTIFY_SECRET");
   console.log(req.response, "REQUEST");
   console.log(code, "setUserAccessTokenModel: CODE");
   console.log(state, "setUserAccessTokenModel: userID");
   axios
     .post(
-      `https://notify-bot.line.me/oauth/token?code=${code}`,
-    //   {
-    //     grant_type: "authorization_code",
-    //     code: code,
-    //     redirect_uri: "https://zudii.tk/api/notify",
-    //     client_id: process.env.NOTIFY_ID,
-    //     client_secret: process.env.NOTIFY_SECRET,
-    //   },
+      `https://notify-bot.line.me/oauth/token?code=${code}&grant_type=authorization_code&redirect_uri=https://zudii.tk/api/notify&client_id=${process.env.NOTIFY_ID}&client_secret=${process.env.NOTIFY_SECRET}`,
+      //   {
+      //     grant_type: "authorization_code",
+      //     code: code,
+      //     redirect_uri: "https://zudii.tk/api/notify",
+      //     client_id: process.env.NOTIFY_ID,
+      //     client_secret: process.env.NOTIFY_SECRET,
+      //   },
       {
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
       }
