92579d3354d8c1d3810035c930c99522bdd17708
Brandon <109305092@g.nccu.edu.tw>
2022-06-27 20:33:42 +0800

    WIP: invalid access token

--- Backend/Controller/Notifyer.js
+++ Backend/Controller/Notifyer.js
@@ -10,22 +10,27 @@ async function setUserAccessToken(req, res) {
   console.log(req, "REQUEST");
   console.log(code, "setUserAccessTokenModel: CODE");
   console.log(state, "setUserAccessTokenModel: userID");
-  const result = await axios.post(
-    "https://notify-bot.line.me/oauth/token",
-    {
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: "https://zudii.tk/api/notify",
-      client_id: process.env.NOTIFY_ID,
-      client_secret: process.env.NOTIFY_SECRET,
-    },
-    {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    }
-  );
-  console.log(result, "ASSSSSSSSSSSSSS")
-
-  setUserAccessTokenModel(result, state);
+  axios
+    .post(
+      "https://notify-bot.line.me/oauth/token",
+      {
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: "https://zudii.tk/api/notify",
+        client_id: process.env.NOTIFY_ID,
+        client_secret: process.env.NOTIFY_SECRET,
+      },
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    )
+    .then((data) => {
+      console.log(data, "DATA");
+      setUserAccessTokenModel(data, state);
+    })
+    .catch((err) => {
+      console.log(err, "ERROR");
+    });
 }
 
 async function getUserAccessToken(userId) {
