926b691696c62da331ab3805f99fb79340a3fd54
Brandon <109305092@g.nccu.edu.tw>
2022-06-30 23:46:40 +0800

    WIP: canel notify

--- Backend/Controller/Notifyer.js
+++ Backend/Controller/Notifyer.js
@@ -35,7 +35,7 @@ async function getUserAccessToken(userId) {
 
 async function cancelNotify(req, res) {
   console.log(req.body)
-  const userId = req.body.userId;
+  const userId = JSON.stringify(req.body.userId);
   const result = cancelNotifyModel(userId);
   if (result.acknowledged === true) {
     res.send({ success: true, status: 200, message: "成功解除通知！" });
