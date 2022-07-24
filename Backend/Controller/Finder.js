35bc3b13b5a504c30314afc30fdc42ec7ca5fc79
Brandon <109305092@g.nccu.edu.tw>
2022-07-25 00:21:15 +0800

    fix : replymessage now distinguish cities from 1, 3 to others

--- Backend/Controller/Finder.js
+++ Backend/Controller/Finder.js
@@ -155,14 +155,14 @@ async function search(req, res) {
                   contents: [
                     {
                       type: "text",
-                      text: "周邊",
+                      text: house.surrounding.type,
                       color: "#999999",
                       size: "md",
                       flex: 1,
                     },
                     {
                       type: "text",
-                      text: `${house.surrounding.type}: ${house.surrounding.desc}`,
+                      text: house.surrounding.desc,
                       wrap: true,
                       color: "#666666",
                       size: "sm",
@@ -178,226 +178,14 @@ async function search(req, res) {
                   contents: [
                     {
                       type: "text",
-                      text: "房型",
+                      text: "距離",
                       color: "#999999",
                       size: "md",
                       flex: 1,
                     },
                     {
                       type: "text",
-                      text: house.type,
-                      wrap: true,
-                      color: "#666666",
-                      size: "sm",
-                      flex: 5,
-                      margin: "none",
-                    },
-                  ],
-                },
-                {
-                  type: "box",
-                  layout: "baseline",
-                  spacing: "sm",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "坪數",
-                      color: "#999999",
-                      size: "md",
-                      flex: 1,
-                    },
-                    {
-                      type: "text",
-                      text: house.size,
-                      wrap: true,
-                      color: "#666666",
-                      size: "sm",
-                      flex: 5,
-                      margin: "none",
-                    },
-                  ],
-                },
-                {
-                  type: "box",
-                  layout: "baseline",
-                  spacing: "sm",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "日期",
-                      color: "#999999",
-                      size: "md",
-                      flex: 1,
-                    },
-                    {
-                      type: "text",
-                      text: house.release_time,
-                      wrap: true,
-                      color: "#666666",
-                      size: "sm",
-                      flex: 5,
-                      margin: "none",
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-        footer: {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
-          contents: [
-            {
-              type: "button",
-              style: "link",
-              height: "sm",
-              action: {
-                type: "uri",
-                label: "詳細資訊",
-                uri: house.link,
-                altUri: {
-                  desktop: house.link,
-                },
-              },
-            },
-            {
-              type: "button",
-              style: "link",
-              height: "sm",
-              action: {
-                type: "uri",
-                label: "查看地點",
-                uri: house.locationLink,
-                altUri: {
-                  desktop: house.locationLink,
-                },
-              },
-            },
-          ],
-          flex: 0,
-        },
-      });
-    });
-  } else if (houses["hasMRT"]) {
-    let messageMRT = "";
-    for (let i = 0; i < house.stations.length; i++) {
-      const station = house.stations[i];
-      messageMRT +=
-        station.stationName.toString() + ` : ${station.distance}公尺`;
-      if (i === 2) {
-        break;
-      } else {
-        messageMRT += "\n";
-      }
-    }
-
-    houses.map((house) => {
-      if (house.size === "Nan") house.size = "沒有資料";
-      else house.size = house.size.toString() + " 坪";
-
-      replyMessages.contents.contents.push({
-        type: "bubble",
-        hero: {
-          type: "image",
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover",
-          action: {
-            type: "uri",
-            uri:
-              house.link ||
-              "https://img1.591.com.tw/house/2022/06/13/165510489014780205.jpg!510x400.jpg",
-          },
-          url:
-            house.imgLink ||
-            "https://img1.591.com.tw/house/2022/06/13/165510489014780205.jpg!510x400.jpg",
-          animated: true,
-        },
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [
-            {
-              type: "text",
-              text: house.title,
-              weight: "bold",
-              size: "lg",
-              margin: "none",
-              wrap: false,
-            },
-            {
-              type: "text",
-              text: req.body.msg,
-              size: "1px",
-              color: "#ffffff",
-            },
-            {
-              type: "box",
-              layout: "vertical",
-              margin: "none",
-              spacing: "sm",
-              contents: [
-                {
-                  type: "box",
-                  layout: "baseline",
-                  margin: "md",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "租金",
-                      color: "#999999",
-                      flex: 1,
-                      size: "md",
-                    },
-                    {
-                      type: "text",
-                      text: house.price.toString() + " 元",
-                      margin: "none",
-                      size: "sm",
-                      flex: 5,
-                    },
-                  ],
-                },
-                {
-                  type: "box",
-                  layout: "baseline",
-                  spacing: "sm",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "地址",
-                      color: "#999999",
-                      size: "md",
-                      flex: 1,
-                    },
-                    {
-                      type: "text",
-                      text: house.location,
-                      wrap: true,
-                      color: "#666666",
-                      size: "sm",
-                      flex: 5,
-                      margin: "none",
-                    },
-                  ],
-                },
-                {
-                  type: "box",
-                  layout: "baseline",
-                  spacing: "sm",
-                  contents: [
-                    {
-                      type: "text",
-                      text: "捷運",
-                      color: "#999999",
-                      size: "md",
-                      flex: 1,
-                    },
-                    {
-                      type: "text",
-                      text: messageMRT,
+                      text: house.surrounding.distance,
                       wrap: true,
                       color: "#666666",
                       size: "sm",
