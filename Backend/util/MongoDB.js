1a6bf89d9de7b7d79aed0c3ca0a7067a22101310
Brandon <109305092@g.nccu.edu.tw>
2022-09-03 20:27:41 +0800

    chore: change collection into dotenv variable

--- Backend/util/MongoDB.js
+++ Backend/util/MongoDB.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 
 const MongoClient = require("mongodb").MongoClient;
 const uri = process.env.MONGO_CONNECTION;
+const MONGO_COLLECTION = process.env.MONGO_COLLECTION;
 const client = new MongoClient(uri, { useNewUrlParser: true });
-const collection = client.db("test").collection("production_591");
+const collection = client.db("test").collection(MONGO_COLLECTION);
 const user = client.db("test").collection("user");
 const mrt = client.db("test").collection("MRT");
 
